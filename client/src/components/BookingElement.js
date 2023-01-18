dece31dc64825cb11b7ceda72adca7440d20b5ee
codeclan student <codeclanstudent@codeclans-MacBook-Pro-4.local>
2023-01-18 16:09:23 +0000

    Style 2 Electric Boogaloo

--- client/src/components/BookingElement.js
+++ client/src/components/BookingElement.js
@@ -4,6 +4,9 @@ import styled from "styled-components";
 const Card = styled.div`
 background-color:#F4F06B;
 width: 50%;
+-webkit-box-shadow: 10px 10px 5px 0px rgba(125,120,57,1);
+-moz-box-shadow: 10px 10px 5px 0px rgba(125,120,57,1);
+box-shadow: 10px 10px 5px 0px rgba(125,120,57,1);
 `
 
 const BookingElement = ({booking, removeBooking}) => {
--- client/src/components/BookingList.js
+++ client/src/components/BookingList.js
@@ -4,6 +4,9 @@ import styled from 'styled-components';
 const BookingTitle = styled.h3`
     background-color:#F4F06B;
     width: 50%;
+    -webkit-box-shadow: 10px 10px 5px 0px rgba(125,120,57,1);
+    -moz-box-shadow: 10px 10px 5px 0px rgba(125,120,57,1);
+    box-shadow: 10px 10px 5px 0px rgba(125,120,57,1);
 `
 
 
