96645bc182dbb43ddef195b0db14ebe688e7235d
codeclan student <codeclanstudent@codeclans-MacBook-Pro-4.local>
2023-01-18 11:34:08 +0000

    Adding seeds

---
+++ server/db/seeds.js
@@ -0,0 +1,24 @@
+const { Db } = require("mongodb");
+
+use booking_hub;
+db.dropDatabase();
+
+db.bookings.insertMany([
+    {
+        name: "Benoit Blanc",
+        email:"benoit@blanc.com",
+        checkedIn: true
+    },
+    {
+        name: "Jack Torrance",
+        email:"jack@outlookhotel.com",
+        checkedIn: true
+    },
+    {
+        name: "Gustave H",
+        email:"conceirge@grandbudapest.com",
+        checkedIn: false
+    }
+
+
+])
