6418af315f5eab26cf8df2b406352a51cf387025
codeclan student <codeclanstudent@codeclans-MacBook-Pro-4.local>
2023-01-18 13:41:58 +0000

    Behave

--- client/src/containers/MainContainer.js
+++ client/src/containers/MainContainer.js
@@ -1,11 +1,5 @@
-<<<<<<< HEAD
 import BookingForm from './components/BookingForm';
 import BookingList from './components/BookingList';
-=======
-
-import BookingForm from '../components/BookingForm';
-import BookingList from '../components/BookingList';
->>>>>>> d48f9d214860808f3ec851f2fbf3d76e6df46264
 import { useState } from 'react';
 
 
