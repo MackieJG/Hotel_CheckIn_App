ae7a1c2ba0d4ed6ff7411c2b774dee20b3edc27a
MackieJG <joshuamackie10@googlemail.com>
2023-01-18 15:13:03 +0000

    updating the chaos

--- client/src/components/BookingElement.js
+++ client/src/components/BookingElement.js
@@ -6,13 +6,17 @@ const BookingElement = ({booking, removeBooking}) => {
             removeBooking(booking._id);
         })
     }
+
+    const changeStatus = () => {
+        updateBooking(booking._id)
+    }
     return (
         <>
         
         
          <p>{booking.name}</p>
          <p>{booking.email}</p>
-         <button>{booking.status ? 'checked in' : 'not checked in'}</button>
+         <button onClick={changeStatus}>{booking.status ? 'checked in' : 'not checked in'}</button>
          <button onClick={handleDelete}>Bolt</button>
         </>
     )
